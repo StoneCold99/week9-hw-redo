a5c16dadd6a0f09579e1af7cbfefba2a58fbe43b
Nelson Zavala <azavala1099@gmail.com>
2023-12-07 11:34:20 -0500

    better version

---
+++ src/components/Cart/Cart.tsx
@@ -0,0 +1,11 @@
+import _React from 'react'; 
+// import React from 'react'; 
+
+
+export const Cart = () => {
+    return (
+        <div>
+            <h1 style={{marginTop: '50px'}}>This is the Shop</h1>
+        </div>
+    )
+}
